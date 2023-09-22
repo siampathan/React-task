9d47a15f386216437970a6a42f0b29065c7d8d4d
siampathan005@gmail.com <siampathan005@gmail.com>
2023-09-23 00:19:57 +0600

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 import App from "./App";
 //import '../src/assets/scss/App.scss';
 import { BrowserRouter } from "react-router-dom";
